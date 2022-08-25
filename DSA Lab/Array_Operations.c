1725dc702f2107c34184df5a9fbbcf9bdd609780
Akash Kumar Singh <akashqp@gmail.com>
2022-08-25 11:58:12 +0530

    DSA Lab 1

--- DSA Lab/Array_Operations.c
+++ DSA Lab/Array_Operations.c
@@ -1,64 +1,124 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
-int creation(int n);
+void creation(int *pos, int n);
 
-int searching(int pos, int n);
+void searching(int *pos, int n);
 
-int traversing(int pos, int n);
+void traversing(int *pos, int n);
+
+void insertion_by_pos(int *pos, int n);
+
+void insertion_by_val(int *pos, int n);
+
+void deletion_by_pos(int *pos, int n);
+
+void deletion_by_val(int *pos, int n);
 
 void main()
 {
-    int n, *pos; char ch;
+    int n, ch, ch1;
     printf("Enter Size of array:");
     scanf("%d",&n);
-    printf("Array operations:\n1.Creation (Compulsory)\n2.Searching\n3.Traversing");
+    int arr[n];
+    printf("Array operations:\n1.Creation (Compulsory)\n2.Insertion\n3.Deletion\n4.Search & Update\n5.Traversing\n6.Exit\n\n");
     while(true)
     {
-        printf("Enter Choice: ");
-        scanf("%c",&ch)
+        printf("Enter Choice:\n");
+        scanf("%d",&ch);
         switch(ch)
         {
-            case '1':   pos=creation(n);
-                        break;
-            case '2':   searching(pos,n);
-                        break;
-            case '3':   traversing(pos,n);
-                        break;
+            case 1: creation(arr,n);
+                    break;
+            case 2: printf("1.Insertion By Position\n2.Insertion By Value\n");
+                    scanf("%d",&ch1);
+                    switch(ch1)
+                    {
+                        case 1: insertion_by_pos(arr,n);
+                                break;
+                        case 2: insertion_by_val(arr,n);
+                                break;
+                        default:printf("Invalid Choice\n");
+                    }
+                    break;
+            case 3: printf("1.Deletion By Position\n2.Deletion By Value\n");
+                    scanf("%d",&ch1);
+                    switch(ch1)
+                    {
+                        case 1: deletion_by_pos(arr,n);
+                                break;
+                        case 2: deletion_by_val(arr,n);
+                                break;
+                        default:printf("Invalid Choice\n");
+                    }
+                    break;
+            case 4: searching(arr,n);
+                    break;
+            case 5: traversing(arr,n);
+                    break;
+            case 6: exit(0);
+
+            default:printf("Invalid Choice\n");
         }
     }
 }
 
-int creation(int n)
+void creation(int *pos, int n)
 {
-    int arr[n];
-    printf("Enter %d elements:\n");
+    printf("Enter %d elements:\n",n);
     for(int i=0;i<n;i++)
     {
-        scanf("%d",&arr[i]);
+        scanf("%d",&(*pos++));
     }
-    return arr;
 }
 
-int searching(int *pos, int n)
+void searching(int *pos, int n)
 {
     int se,count=0;
     printf("Enter Search Element:");
     scanf("%d",&se);
     for(int i=0; i<n; i++, pos++)
     {
-        if(arr[pos]==se)
+        if(*pos==se)
             count++;
     }
     if(count>0)
-        printf("Search Element present %d times in array\n",count)
+        printf("Search Element present %d times in array\n",count);
     else
         printf("Search Element not present in array\n");
 }
 
-int traversing(int *pos, int n)
+void traversing(int *pos, int n)
 {
     for(int i=0;i<n;i++,pos++)
     {
-        printf("%d\t",arr[pos]);
+        printf("%d\t",*pos);
     }
+    printf("\n");
+}
+
+void insertion_by_pos(int *pos, int n)
+{
+    int val, pos;
+    printf("Enter Value to be Inserted:");
+    scanf("%d",&val);
+    printf("Enter Position to Insert the Value at:");
+    scanf("%d",&pos);
+    
+}
+
+void insertion_by_val(int *pos, int n)
+{
+
+}
+
+void deletion_by_pos(int *pos, int n)
+{
+
+}
+
+void deletion_by_val(int *pos, int n)
+{
+
 }
